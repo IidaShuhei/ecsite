698c75f20e40c382622fe01b83bd6145660474ef
IidaShuhei <shuhei.iida@rakus-partners.co.jp>
2020-06-07 16:29:20 +0900

    最新

--- src/main/java/com/example/controller/DeleteItemController.java
+++ src/main/java/com/example/controller/DeleteItemController.java
@@ -1,7 +1,6 @@
 package com.example.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +16,7 @@ public class DeleteItemController {
 	private DeleteService deleteService;
 	
 	@RequestMapping("")
-	@CrossOrigin()
 	public void delete(@RequestBody OrderItemForm orderItemForm) {
-		
 		deleteService.delete(orderItemForm.getItemId());
 	}
 }
--- src/main/java/com/example/controller/ShowShoppingCartController.java
+++ src/main/java/com/example/controller/ShowShoppingCartController.java
@@ -3,7 +3,6 @@ package com.example.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +17,6 @@ public class ShowShoppingCartController {
 	private ShowShoppingCartService showShoppingCartService;
 	
 	@RequestMapping("")
-	@CrossOrigin()
 	public List<Order> showShoppingCart() {
 		return showShoppingCartService.showShoppingCart(1, 0);
 	}
